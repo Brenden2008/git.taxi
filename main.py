e30f368d9dbbd3dda670fa60631a460a21a1d9f9
Brenden Stahle <brendenstahle@gmail.com>
2022-10-27 15:20:07 -0500

    Currently Broken Need to fix the url matching system

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from urllib.parse import urlsplit, urlunsplit
 
 from deta import Deta
 
@@ -12,7 +13,26 @@ app = FastAPI()  # create app instance
 
 deta = Deta("")  # provide project key
 
-db = deta.Base("gittaxi")  # create a DetaBase
+db = deta.Base("gittaxi_links")  # create a DetaBase
+
+allowed_domains = [
+    "github.io",
+    "github.com",
+    "githubapp.com",
+    "githubusercontent.com",
+    "githubassets.com",
+    "git.io",
+    "gitlab.com",
+    "gitlab.io",
+    "codeberg.page",
+    "codeberg.org",
+]
+
+def is_root(url):
+    head, sep, tail = url.partition('//')
+    is_root_domain = tail.split('/', 1)[0] if '/' in tail else url
+    # printing or returning is_root_domain will give you what you seek
+    print(is_root_domain)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -26,10 +46,13 @@ async def get_home(request: Request):
 
 @app.post("/shorten")
 async def shorten_url(url):
-    # data = jsonable_encoder(url)
-    token = secrets.token_urlsafe(6)
-    db.put(url, key=token)
-    return {"url": token}
+    if is_root(url) in allowed_domains:
+        # data = jsonable_encoder(url)
+        token = secrets.token_urlsafe(6)
+        db.put(url, key=token)
+        return {"url": token, "allowed_url": "true"}
+    else:
+        return {"allowed_url": "false"}
 
 
 @app.get("/{url}")
--- static/index.js
+++ static/index.js
@@ -29,11 +29,38 @@ function createAndAppendUrl(url, shortUrl) {
     urlsContainer.appendChild(span);
 }
 
+function createAndAppendText(text) {
+
+    // create elements
+    let span = document.createElement("span");
+    let p1 = document.createElement("p");
+
+    // set element text values
+    p1.innerHTML = text;
+
+    // add a as child of span
+    span.appendChild(p1);
+
+    // add style class to span
+    span.classList.add("short");
+
+    // get parent URLs container
+    const urlsContainer = document.getElementById("shorts");
+
+    urlsContainer.appendChild(span);
+}
+
 async function shortenUrl(url) {
     let res = await fetch(`shorten?url=${url}`, {method: "POST"});
     let data = await res.json();
 
-    createAndAppendUrl(url, `${location.href}${data.url}`);
+    if(data.allowed_url == "true"){
+        createAndAppendUrl(url, `${location.href}${data.url}`);
+    } else {
+        createAndAppendText(`
+            This is not an allowed domain. You can see the list of allowed domains <a href="/static/allowed_domains.txt">here</a>.
+        `)
+    }
 }
 
 formEl.addEventListener("submit", (event) => {
