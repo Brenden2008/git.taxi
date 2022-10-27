3f498069091f9ad969d05251e80a4b5b5bc6942f
Brenden Stahle <brendenstahle@gmail.com>
2022-10-27 18:34:38 -0500

    1

--- static/index.js
+++ static/index.js
@@ -6,19 +6,19 @@ function createAndAppendUrl(url, shortUrl) {
     // create elementss
     let span = document.createElement("span");
     let p1 = document.createElement("a");
-    //let p2 = document.createElement("a");
+    let p2 = document.createElement("p");
 
     // set element text values
     p1.innerText = shortUrl;
-    //p2.innerText = url;
+    p2.innerText = url;
 
     // set element href values
     p1.href = shortUrl;
     //p2.href = url;
 
     // add a as child of span
+    span.appendChild(p2);
     span.appendChild(p1);
-    //span.appendChild(p2);
 
     // add style class to span
     span.classList.add("short");
