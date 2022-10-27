3e547092d9b414783456c574371f0acc0c129f75
Brenden Stahle <brendenstahle@gmail.com>
2022-10-27 12:50:53 -0500

    2nd commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# git.taxi
-The git.taxi service is a link shortener like git.io powered by deta.sh.
+<center>
+<h1> MiniShort </h1>
+
+<p> A damn simple URL shortener in Python on Deta </p>
+</center>
+
+This is a simple URL shortener made using python and runs on deta.
+
+## Technologies Used
+- FastAPI
+- Deta
+
+## Dependencies
+- FastAPI
+- aiofiles
+- jinja2
+
+## How it works
+
+You open the main site and enter your URL in the form. When you press submit, JavaScript takes care of making an asynchronous request to the underlying server which calls Deta Base to add the URL to the Base.
+When you make a request to a short URL, it looks for that which matches in the database and responds with a `RedirectResponse` to the *target*
+
+Live app [here](https://d4kqx2.deta.dev/)
+
+<center>
+<p> Live a star if you like this </p>
+<p> Made by Josias Aurel </p>
+<center>
---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from deta import Deta
+
+import secrets
+
+app = FastAPI()  # create app instance
+
+deta = Deta("")  # provide project key
+
+db = deta.Base("gittaxi")  # create a DetaBase
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+pages = Jinja2Templates(directory="pages")
+
+
+@app.get("/")
+async def get_home(request: Request):
+    return pages.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/shorten")
+async def shorten_url(url):
+    # data = jsonable_encoder(url)
+    token = secrets.token_urlsafe(6)
+    db.put(url, key=token)
+    return {"url": token}
+
+
+@app.get("/{url}")
+async def redirect_to_(url):
+    red_ = db.get(url)
+    print(red_)
+    return RedirectResponse(red_["value"])
